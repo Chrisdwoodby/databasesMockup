825791380e91e8024f84104682344c5eb01f53d8
chrisdwoodby <chrisdwoodby@gmail.com>
2021-07-10 09:09:47 -0500

    complete designing multi-table schema

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,12 @@
 var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chat'
+})
+
+connection.connect()
 
 // Create a database connection and export it from this file.
 // Confirm that the credentials supplied for the connection are correct.
--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,29 @@
 CREATE DATABASE chat;
-
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+
+CREATE TABLE users (
+  USERID int NOT NULL,
+  UserName varchar (200) NOT NULL,
+  PRIMARY KEY (USERID)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  ROOMID int NOT NULL,
+  Roomname varchar (200) NOT NULL,
+  PRIMARY KEY (ROOMID)
+);
 
-
-
+CREATE TABLE messages (
+  ID int NOT NULL,
+  Messages varchar (255) NOT NULL,
+  USERID int NOT NULL,
+  ROOMID int NOT NULL,
+  PRIMARY KEY (ID),
+  CONSTRAINT fk_users FOREIGN KEY (USERID) REFERENCES users (USERID),
+  CONSTRAINT fk_rooms FOREIGN KEY (ROOMID) REFERENCES rooms (ROOMID)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
